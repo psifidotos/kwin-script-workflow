65845acaee1744a0afac6d122afdfe27d1a66af9
Michail Vourlakos <mvourlakos@gmail.com>
2013-03-09 19:41:26 +0200

    deleted files which are plasmoid branch responsibility

--- qdbus/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#create adaptor
-qdbuscpp2xml -M -S Store.h -o org.opentoolsandspace.WorkareaManager.xml
-#create client access
-cd client
-qdbusxml2cpp -v -c StoreInterface -p storeinterface.h:storeinterface.cpp ../org.opentoolsandspace.WorkareaManager.xml
